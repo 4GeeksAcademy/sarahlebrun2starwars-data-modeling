cae16064b39ab1b057680db9eff9a254021e3343
Sarahlebrun2 <130923257+Sarahlebrun2@users.noreply.github.com>
2023-08-30 18:12:12 +0000

    finish diagram

--- src/models.py
+++ src/models.py
@@ -7,24 +7,51 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Character(Base):
+    __tablename__ = 'character'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
+    name =Column(String(250), nullable=False)
+    hair_color =Column(String(250), nullable=True)
+    birth_year =Column(String(250), nullable=True)
+    gender =Column(String(250), nullable=True)
+    eye_color =Column(String(250), nullable=True)
+    height =Column(String(250), nullable=True)
+    mass =Column(String(250), nullable=True)
+    skin_color =Column(String(250), nullable=True)
+class Plante (Base):
+    __tablename__ = 'plante'
+    # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    diameter =Column(String(250), nullable=False)
+    rotation_period =Column(String(250),nullable=True)
+    gravity =Column(String(250), nullable=True)
+    population =Column(String(250), nullable=True)
+    climate =Column(String(250), nullable=True)
+    surface_water =Column(String(250), nullable=True)
+    orbital_period =Column(String(250), nullable=True)
 
+
+
+class User(Base):
+    __tablename__="user"
+    id =Column(Integer, primary_key=True)
+    username =Column(String(250), nullable=False)
+    firstname =Column(String(250), nullable=False)
+    lastname =Column(String(250), nullable=True)
+    email =Column(String(250), nullable=False)
+
+class Favorites(Base):
+    __tablename__="favorites"
+    id = Column(Integer, primary_key=True)
+    user=relationship(User)
+    user_id=Column(Integer, ForeignKey("user.id"))
+    character=relationship(Character)
+    character_id=Column(Integer, ForeignKey("character.id"))
+    plante=relationship(Plante)
+    plante_id=Column(Integer, ForeignKey("plante.id"))
     def to_dict(self):
         return {}
 
